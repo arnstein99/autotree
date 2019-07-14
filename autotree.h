cd4ede2070f3e93d22e40843197e2aa5caeb426a
David Arnstein <arnstein@pobox.com>
2019-07-14 11:09:37 -0700

    Starting work on iterators.

--- autotree.h
+++ autotree.h
@@ -66,6 +66,11 @@ public:
     // TODO: provide useful return value
     void insert (const Key& key, const Tp& val);
 
+    typedef std::pair<const Key, Tp> value_type;
+    class depth_first_iterator;
+    depth_first_iterator start();
+    depth_first_iterator end();
+
 private:
 
     bool expand (
@@ -74,6 +79,29 @@ private:
     Key mBase;
 
 }; // class Tree
+
+template <typename Key,
+          typename Tp,
+          typename Parent,
+          typename Compare,
+          typename Equ>
+class Tree<Key,Tp,Parent,Compare,Equ>::depth_first_iterator
+{
+public:
+
+    depth_first_iterator();
+    depth_first_iterator(depth_first_iterator const& other);
+    depth_first_iterator& operator=(depth_first_iterator const& other);
+    depth_first_iterator(depth_first_iterator&& other);
+    depth_first_iterator& operator=(depth_first_iterator&& other);
+    ~depth_first_iterator();
+
+    depth_first_iterator next() const;
+    depth_first_iterator& operator++();
+    depth_first_iterator& operator++(int);
+    value_type operator->();
+
+}; // class Tree::depth_first_iterator
 
 } // namespace AutoTree
 
